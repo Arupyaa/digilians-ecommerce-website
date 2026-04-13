f6abbe17bfa761ffc7980d8d304b9d7902371ff6
Arupyaa <amkeshk98@gmail.com>
2026-04-13 14:34:22 +0200

    moved ProductSection logic into ProductList component

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-import ProductCard from "./ProductCard";
+import ProductsList from "./ProductsList";
 
 
-function ProductSection({ productCount,productRow, productCategory = "" }) {
-    const [productList, changeProductList] = useState([]);
-    async function getProducts() {
-        let result = await fetch(`https://x8ki-letl-twmt.n7.xano.io/api:6nBUGF81/products?category=${productCategory}`).then(response => response.json());
-        changeProductList(result.slice(0, productCount));
-        console.log(productList);
-
-    }
-    useEffect(() => { getProducts() }, []);
-    return (
-        <div className={`mx-auto w-[90%] md:w-[78%] flex flex-col md:grid md:grid-flow-col md:grid-rows-${productRow} gap-y-[20px] md:gap-[40px] py-[30px] md:py-[80px] justify-between`}>
-            {productList.map((product) => {
-                return <ProductCard productTitle={product.title} productPrice={product.price} productDiscountedPrice={product.discount_price} productImage={product.main_image.url} />
-            })}
-        </div>
+function ProductSection({ productCount, productRow, productCategory }) {
+    return(
+        <ProductsList productCount={productCount} productRow={productRow} productCategory={productCategory} />
     );
 }
 export default ProductSection;
