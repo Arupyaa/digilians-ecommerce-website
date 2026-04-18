b2d66f6f07c097fa7137bcb67ad76074b0842cf0
Arupyaa <amkeshk98@gmail.com>
2026-04-18 17:48:16 +0200

    added routing to home category links

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 
 
-function CategoryCard({backgroundImage,categoryName}){
-    return(
-        <div className="relative w-[400px]">
-            <img className="w-full aspect-[5/3] block z-1 rounded-md" src={backgroundImage} alt="gpu image" />
-            <div className="absolute rounded-md inset-0 bg-gradient-to-t from-black/70 to-transparent" />
-            <h3 className="text-white z-10 text-2xl absolute bottom-6 left-10 font-bold">{categoryName}</h3>
-        </div>
+function CategoryCard({ backgroundImage, categoryName, category }) {
+    return (
+        <Link to={`products/${category}/page/1`}>
+            <div className="relative w-[400px]">
+                <img className="w-full aspect-[5/3] block z-1 rounded-md" src={backgroundImage} alt="gpu image" />
+                <div className="absolute rounded-md inset-0 bg-gradient-to-t from-black/70 to-transparent" />
+                <h3 className="text-white z-10 text-2xl absolute bottom-6 left-10 font-bold">{categoryName}</h3>
+            </div>
+        </Link>
     );
 }
 
--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -6,9 +6,9 @@ import keyboardImage from "../assets/keyboard.jpg"
 function CategorySection() {
     return (
         <div className="mx-auto w-[90%] md:w-[78%] flex flex-col md:flex-row gap-[20px] md:gap-[40px] py-[30px] md:py-[60px] md:justify-between">
-            <CategoryCard backgroundImage={gpuImage} categoryName="Browse GPUs"/>
-            <CategoryCard backgroundImage={monitorImage} categoryName="Browse Monitors"/>
-            <CategoryCard backgroundImage={keyboardImage} categoryName="Browse PC Accessories"/>
+            <CategoryCard backgroundImage={gpuImage} categoryName="Browse GPUs" category={"gpu"} />
+            <CategoryCard backgroundImage={monitorImage} categoryName="Browse Monitors" category={"monitor"} />
+            <CategoryCard backgroundImage={keyboardImage} categoryName="Browse PC Accessories" category={"keyboard"} />
         </div>
     );
 }
